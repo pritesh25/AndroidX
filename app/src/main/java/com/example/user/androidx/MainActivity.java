011c00c5202e190367a18a181b03a0383bfe09f5
pritesh <priteshvishwakarma@gmail.com>
2018-12-28 10:27:21 +0530

    build gradle updated

--- app/src/main/java/com/example/user/androidx/MainActivity.java
+++ app/src/main/java/com/example/user/androidx/MainActivity.java
@@ -1,45 +1,28 @@
 package com.example.user.androidx;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.os.Bundle;
-import android.view.Display;
-import android.widget.TextView;
-
-import com.example.user.androidx.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerview;
     private RecyclerViewAdapter recyclerViewAdapter;
-    private String[] data = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19"};
-
-    private TextView tv1;
-    private ActivityMainBinding activityMainBinding;
+    private String[] data = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
-        activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        recyclerview = findViewById(R.id.recyclerview);
+        recyclerview.setHasFixedSize(true);
+        recyclerview.setLayoutManager(new LinearLayoutManager(this));
 
-        ModelData data = new ModelData();
-        data.setCelsius("27");
-        activityMainBinding.setTv1(data);
-
-//        recyclerview = findViewById(R.id.recyclerview);
-//        recyclerview.setHasFixedSize(true);
-//        recyclerview.setLayoutManager(new LinearLayoutManager(this));
-//
-//        recyclerViewAdapter = new RecyclerViewAdapter(this,data);
-//        recyclerview.setAdapter(recyclerViewAdapter);
-
-        tv1 = findViewById(R.id.tv1);
-
+        recyclerViewAdapter = new RecyclerViewAdapter(this,data);
+        recyclerview.setAdapter(recyclerViewAdapter);
 
     }
 }
