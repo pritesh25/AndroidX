32daa65e64b79c19688c7e8f11462a903ac14ea1
pritesh <priteshvishwakarma@gmail.com>
2018-12-29 18:30:59 +0530

    status window added

--- app/src/main/java/com/example/user/androidx/MainActivity.java
+++ app/src/main/java/com/example/user/androidx/MainActivity.java
@@ -6,15 +6,21 @@ import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.ResolveInfo;
+import android.graphics.Color;
+import android.graphics.PixelFormat;
 import android.graphics.drawable.Drawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -38,7 +44,7 @@ public class MainActivity extends AppCompatActivity implements ConnectionStateMo
     private static final String TAG = MainActivity.class.getSimpleName();
     private RecyclerView recyclerview;
     private RecyclerViewAdapter recyclerViewAdapter;
-    private String[] data = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"};
+    private String[] data = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"};
 
 
     private static final boolean SHOW_SPEED_IN_BITS = false;
@@ -48,25 +54,58 @@ public class MainActivity extends AppCompatActivity implements ConnectionStateMo
     private static boolean wifiConnected = false;
     private static boolean mobileConnected = false;
 
-    //how is this ?
-
-
-
-    //pritesh patil from nandurbar
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        List<AppList> installedApps = getInstalledApps();
+        //List<AppList> installedApps = getInstalledApps();
 
-        gridViewSetup(installedApps);
+        //gridViewSetup(installedApps);
 
         //recyclerViewSetup(installedApps);
 
-        tv_network = findViewById(R.id.tv_network);
+        //networkMonitor();
 
+        statusBarExample();
+
+    }
+
+    private void statusBarExample() {
+
+        int statusBarHeight = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) statusBarHeight = getResources().getDimensionPixelSize(resourceId);
+
+        final WindowManager.LayoutParams parameters = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                statusBarHeight,
+                WindowManager.LayoutParams.TYPE_SYSTEM_ERROR,   // Allows the view to be on top of the StatusBar
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN,    // Keeps the button presses from going to the background window and Draws over status bar
+                PixelFormat.TRANSLUCENT);
+        parameters.gravity = Gravity.TOP | Gravity.CENTER;
+
+        LinearLayout ll = new LinearLayout(this);
+        ll.setBackgroundColor(Color.TRANSPARENT);
+        LinearLayout.LayoutParams layoutParameteres = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        ll.setLayoutParams(layoutParameteres);
+
+        TextView tv = new TextView(this);
+        ViewGroup.LayoutParams tvParameters = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        tv.setLayoutParams(tvParameters);
+        tv.setTextColor(Color.WHITE);
+        tv.setGravity(Gravity.CENTER);
+        tv.setText("123");
+        ll.addView(tv);
+
+        WindowManager windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        windowManager.addView(ll, parameters);
+
+    }
+
+    private void networkMonitor() {
+
+        tv_network = findViewById(R.id.tv_network);
         //check for connection
         new ConnectionStateMonitor(this).enable(getApplicationContext());
         //monitor the speed
@@ -77,8 +116,8 @@ public class MainActivity extends AppCompatActivity implements ConnectionStateMo
 
     private void gridViewSetup(List<AppList> installedApps) {
 
-        CustomListAdapter adapter = new CustomListAdapter(this,installedApps);
-        GridView gridView  = (GridView) findViewById(R.id.gridView);
+        CustomListAdapter adapter = new CustomListAdapter(this, installedApps);
+        GridView gridView = (GridView) findViewById(R.id.gridView);
         gridView.setAdapter(adapter);
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -92,8 +131,7 @@ public class MainActivity extends AppCompatActivity implements ConnectionStateMo
 
     }
 
-    private void recyclerViewSetup(List<AppList> installedApps)
-    {
+    private void recyclerViewSetup(List<AppList> installedApps) {
 //        recyclerview = findViewById(R.id.recyclerview);
 //        recyclerview.setHasFixedSize(true);
 //        recyclerview.setLayoutManager(new LinearLayoutManager(this));
@@ -108,7 +146,7 @@ public class MainActivity extends AppCompatActivity implements ConnectionStateMo
 
         Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
         mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
-        List<ResolveInfo> packs = getPackageManager().queryIntentActivities( mainIntent, 0);
+        List<ResolveInfo> packs = getPackageManager().queryIntentActivities(mainIntent, 0);
 
         for (int i = 0; i < packs.size(); i++) {
             ResolveInfo p = packs.get(i);
@@ -132,15 +170,15 @@ public class MainActivity extends AppCompatActivity implements ConnectionStateMo
 
     @Override
     public void onConnected() {
-        Log.d(TAG,"onConnected");
-        Toast.makeText(getApplicationContext(),"onConnected",Toast.LENGTH_LONG).show();
+        Log.d(TAG, "onConnected");
+        Toast.makeText(getApplicationContext(), "onConnected", Toast.LENGTH_LONG).show();
         checkNetworkConnection();
     }
 
     @Override
     public void onDisconnected() {
-        Log.d(TAG,"onDisconnected");
-        Toast.makeText(getApplicationContext(),"onDisconnected",Toast.LENGTH_LONG).show();
+        Log.d(TAG, "onDisconnected");
+        Toast.makeText(getApplicationContext(), "onDisconnected", Toast.LENGTH_LONG).show();
     }
 
     private ITrafficSpeedListener mStreamSpeedListener = new ITrafficSpeedListener() {
@@ -184,9 +222,9 @@ public class MainActivity extends AppCompatActivity implements ConnectionStateMo
         if (activeInfo != null && activeInfo.isConnected()) {
             wifiConnected = activeInfo.getType() == ConnectivityManager.TYPE_WIFI;
             mobileConnected = activeInfo.getType() == ConnectivityManager.TYPE_MOBILE;
-            if(wifiConnected) {
+            if (wifiConnected) {
                 Log.d(TAG, getResources().getString(R.string.wifi_connection));
-            } else if (mobileConnected){
+            } else if (mobileConnected) {
                 Log.d(TAG, getResources().getString(R.string.mobile_connection));
             }
         } else {
